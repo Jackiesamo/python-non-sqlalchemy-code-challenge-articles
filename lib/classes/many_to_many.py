1b065ed8dc9b2782aa5d244b08093c1ce4e03175
jacklyne <jacklyne.owuor@student.moringaschool>
2025-11-17 08:34:22 +0300

    added content in class article

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -3,3 +3,36 @@ class Article:
         self.author = author
         self.magazine = magazine
         self.title = title
+
+class Author:
+    def _init_(self, name):
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if hasattr(self, "_name"):
+            return
+        if not isinstance(value, str):
+            return
+        if len(value) == 0:
+            return
+        self._name = value
+
+    def articles(self):
+        return [article for article in Article.all if article.author == self]
+
+    def magazines(self):
+        return list({article.magazine for article in self.articles()})
+
+    def add_article(self, magazine, title):
+        return Article(self, magazine, title)
+
+    def topic_areas(self):
+        topics = {mag.category for mag in self.magazines()}
+        return list(topics) if topics else None       
+
+
