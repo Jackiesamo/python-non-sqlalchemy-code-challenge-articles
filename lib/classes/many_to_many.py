ee79aba0ca00f7ceded2d668f9bf89ee2c6cf2cd
jacklyne <jacklyne.owuor@student.moringaschool>
2025-11-17 08:43:22 +0300

    addded article

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -81,6 +81,16 @@ class Author:
         ]
         return authors if authors else None
     
+    class Article:
+     all = []
+
+    def _init_(self, author, magazine, title):
+        self.author = author
+        self.magazine = magazine
+        self.title = title
+        Article.all.append(self)
+
+    
     
           
 
